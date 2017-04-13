964cee8bdacb9ce2c354a88d8b91d8fbad25c301
jan <azizjan819gmail.com>
2017-04-13 19:12:56 +0900

    new

---
+++ src/main/java/jp/co/topgate/jan/web/HttpRequest.java
@@ -0,0 +1,10 @@
+package jp.co.topgate.jan.web;
+
+/**
+ * Created by aizijiang.aerken on 2017/04/13.
+ */
+public class HttpRequest {
+
+    public HttpRequest(String reQ) {
+    }
+}
---
+++ src/main/java/jp/co/topgate/jan/web/HttpResponse.java
@@ -0,0 +1,15 @@
+package jp.co.topgate.jan.web;
+
+/**
+ * Created by aizijiang.aerken on 2017/04/13.
+ */
+public class HttpResponse {
+
+    HttpRequest request ;
+
+    public HttpResponse(HttpRequest request){
+
+        this.request = request ;
+
+    }
+}
