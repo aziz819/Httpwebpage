cc72dcdad61ba332c416da6820783c9a69036c01
jan <azizjan819gmail.com>
2017-04-15 17:59:46 +0900

    JUnitTest設定

---
+++ src/test/java/jp/co/topgate/jan/web/MainTest.java
@@ -0,0 +1,15 @@
+package jp.co.topgate.jan.web;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by aizijiang.aerken on 2017/04/15.
+ */
+public class MainTest {
+
+    @Test
+    public void runserver(){
+        Assert.assertTrue(true);
+    }
+}
